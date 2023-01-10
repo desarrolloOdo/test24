138016f44c2624ef59660ec4a2f11e522a28e84d
ivan <odootest725@gmail.com>
2023-01-10 20:10:40 +0000

    [ADD] Modulocreado

---
+++ ivan/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary':"""Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to manage Training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author': 'Ivan',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+       
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    
+    'license': 'LGPL-3',
+}
